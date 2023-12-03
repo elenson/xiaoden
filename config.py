e30c40ede14855dff1090936af015b187ff139d6
elenson <elenson@huami.com>
2023-12-04 01:12:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ DEBUG = True
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'root')
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+# 读取阿里云OSS环境变量
+oss_access_key_id = os.environ.get("OSS_ACCESS_KEY_ID", 'your_oss_access_key_id')
+oss_access_key_secret = os.environ.get("OSS_ACCESS_KEY_SECRET", 'your_oss_access_key_secret')
+oss_endpoint = os.environ.get("OSS_ENDPOINT", 'http://your-endpoint.com')
+oss_bucket_name = os.environ.get("OSS_BUCKET_NAME", 'your-bucket-name')
